4630605dd4baa0bcb43ab60ea2172dbd9e3c7abc
Tina Kovalova <kovalevatkv@gmail.com>
2023-04-05 17:50:47 +0300

    responsive testimonials

--- client/src/components/testimonial/Testimonial.jsx
+++ client/src/components/testimonial/Testimonial.jsx
@@ -12,13 +12,14 @@ export function Testimonial() {
             name="Sara Taylor"
             text="Simply dummy text of the printing and typesetting industry. Lorem Ipsum simply dummy text of the printing and typesetting industry. Lorem Ipsum has been."
             position="Consumer"
+            rating="5"
           />
         </div>
         <div className="testimonial__stat">
           <CircleSign amount="100%" productName="Organic" />
-          <CircleSign amount="100%" productName="Organic" />
-          <CircleSign amount="100%" productName="Organic" />
-          <CircleSign amount="100%" productName="Organic" />
+          <CircleSign amount="285" productName="Active Product" />
+          <CircleSign amount="350+" productName="Organic Orchads" />
+          <CircleSign amount="25+" productName="Years of Farming" />
         </div>
       </div>
     </section>
