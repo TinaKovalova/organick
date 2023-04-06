0ca2789bae2eae9e2d34a3defe4dea279fa94e85
Tina Kovalova <kovalevatkv@gmail.com>
2023-04-06 14:08:54 +0300

    added hover-effect

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config({path: path.resolve(__dirname, '.env')});
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 
 const app = express();
